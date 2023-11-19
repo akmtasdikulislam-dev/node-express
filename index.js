a503efe854efd8be5cf025c38278fe6c8e4850cd
akmtasdikulislam-dev <akmtasdikulislam-dev@outlook.com>
2023-11-20 01:45:38 +0600

    /driver-list api end-point added to retrieve all drivers from Database

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://organicUser:hdZcg4EYZGUfJQ8w@cluster0.dkmkvf0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/driver-list", async (req, res) => {
+  const database = await client.db("DriverMonitoringSystem");
+  const driverList = await database.collection("driverList");
+  const allDriverCursor = await driverList.find({});
+  const allDriver = await allDriverCursor.toArray();
 
+  res.send(allDriver);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
