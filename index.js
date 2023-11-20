34583c940e5e800f554a5c28629c78a5f49ee638
akmtasdikulislam-dev <akmtasdikulislam-dev@outlook.com>
2023-11-20 14:02:20 +0600

    updated mongoDB atas connection process

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://organicUser:hdZcg4EYZGUfJQ8w@cluster0.dkmkvf0.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri = `mongodb+srv://organicUser:hdZcg4EYZGUfJQ8w@cluster0.dkmkvf0.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+const MongoClient = require("mongodb").MongoClient;
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://organicUser:hdZcg4EYZGUfJQ8w@cluster0.dkmkvf0.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
